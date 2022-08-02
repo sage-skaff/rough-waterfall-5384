8496b044ec642bf6687ca4759088e866a94ee312
Sage Skaff <skaffsage@gmail.com>
2022-08-02 14:43:03 -0700

    Add test for view all items link

--- spec/features/supermarkets/show_spec.rb
+++ spec/features/supermarkets/show_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe 'supermarket show page' do
       expect(page).to_not have_content('Buy Things')
     end
   end
+
+  it 'has link to supermarkets item index page' do
+    market = Supermarket.create!(name: 'Fresh Market', location: 'Central Town Center')
+    market2 = Supermarket.create!(name: 'Buy Things', location: 'Central Town Center')
+
+    item1 = market.items.create!(name: 'One Egg', price: 1)
+    item2 = market.items.create!(name: 'Pineapple', price: 4)
+    item3 = market2.items.create!(name: 'Baguette', price: 3)
+
+    visit "/supermarkets/#{market.id}"
+
+    within '#view-items-link' do
+      expect(page).to have_link('View All Items')
+    end
+  end
 end
