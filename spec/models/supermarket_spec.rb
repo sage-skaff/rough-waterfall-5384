6d99726a4d13771397390a3fa688475670cca810
Sage Skaff <skaffsage@gmail.com>
2022-08-02 13:46:13 -0700

    Add relationships

--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Supermarket, type: :model do
   describe 'relationships' do
     it { should have_many :customers }
+    it { should have_many :items }
   end
 end
+
